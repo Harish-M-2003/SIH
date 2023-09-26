6d81848bb6d6993da333d06f4ed37cbf08cc282c
harishm <harishm762003@gmail.com>
2023-09-26 17:58:02 +0530

    In Contest page search filter should be fixed  , remaning thing in contest page and details page is compoleted.

--- Pages/Contest/Contest.tsx
+++ Pages/Contest/Contest.tsx
@@ -4,15 +4,22 @@ import {
     Text, 
     TextInput,
     ScrollView ,
+    TouchableOpacity ,
 } from "react-native";
+
+import {useState} from "react";
+
 import ContestCard from "./components/ContestCard";
 import BottomNavigation from "../../AppComponent/BottomNavigation";
 import { DATA } from "../../data/data";
 
 export default function Contest({navigation}){
+
+    const [currentTab , setCurrentTab] = useState(0);
+
     return (
         <View style={{flex : 1}}>
-            <ScrollView style={styles.container}>
+            <ScrollView style={styles.container} showsVerticalScrollIndicator = {false}>
                 <Text style={styles.title}>Contests</Text>
                 <TextInput 
                     placeholder="search for contests" 
@@ -25,6 +32,38 @@ export default function Contest({navigation}){
                         marginVertical : "5%",
                     }}
                 />
+                <View style={{
+                    flexDirection : "row" ,
+                    justifyContent : "space-between" ,
+                    borderWidth : 1 ,
+                    borderColor : "rgba(0,0,0,0.1)",
+                    borderRadius : 5,
+                }}>
+                    <TouchableOpacity 
+                        onPress={() => setCurrentTab(0)}
+                        style={{
+                        flex : 1 ,
+                        padding : "5%" ,
+                        alignItems : "center" , 
+                        borderTopLeftRadius  : 5,
+                        borderBottomLeftRadius  : 5,
+                        backgroundColor : (currentTab == 0)? "rgba(107, 28, 176,1)" : "transparent"
+                    }} >
+                        <Text style={{fontWeight : "bold" , color : (currentTab == 0)? "white" : "black"}}>All Contests</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={() => setCurrentTab(1)}
+                        style={{ 
+                            flex : 1 , 
+                            padding : "5%" , 
+                            alignItems : "center",
+                            borderTopRightRadius  : 5,
+                            borderBottomRightRadius  : 5,
+                            backgroundColor : (currentTab != 0)? "rgba(107, 28, 176,1)" : "transparent"
+                    }}>
+                        <Text style={{fontWeight : "bold" , color : (currentTab != 0)?"white" : "black"}}>Your Contest</Text>
+                    </TouchableOpacity>
+                </View>
                 {
                     DATA.map((value , index) => (
                         <ContestCard 
@@ -37,6 +76,7 @@ export default function Contest({navigation}){
                         />
                     ))
                 }
+                <View style={{height : 90}}/>
             </ScrollView>
             <BottomNavigation navigation={navigation}/>
         </View>
